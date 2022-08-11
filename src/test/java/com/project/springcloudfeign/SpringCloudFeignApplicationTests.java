82923e8d2888b6d89a728e47b14800fea3be332a
Emerson Millard <emillard.code@gmail.com>
2022-08-11 15:50:56 -0700

    Initial commit

---
+++ src/test/java/com/project/springcloudfeign/SpringCloudFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springcloudfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
